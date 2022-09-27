a54aa89d963743d5c2c4a6138cc9516c74adaf1e
Somber <dj2548@gmail.com>
2022-09-27 15:57:52 -0400

    2nd commit

---
+++ lab_2 electric boogaloo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+bool run = true;
+while (run)
+{
+    Console.WriteLine("What is your name?");
+    string name = Console.ReadLine();
+    Console.WriteLine(" Enter a number 1-100 \n");
+    int num = int.Parse(Console.ReadLine());
+    
+   if (num < 60 && num % 2 == 1)
+    {
+        Console.WriteLine(" Your number is odd and under 60 \n");
+    }
+     else if (num > 60 && num % 2 == 1) 
+    {
+        Console.WriteLine(" Your number is odd and above 60 \n");
+    }   
+    else if (num < 60 && num % 2 == 0)
+    { 
+            Console.WriteLine(" Your number is even and under 60 \n");
+    }
+       if (num % 2 == 0 && num >=2 && num <= 24)
+    {
+        Console.WriteLine(" This number is even and between 2 and 24 exclusive \n");
+
+    }
+      else if (num % 2 == 0 && num >= 2 || num <= 100)
+    {
+        Console.WriteLine(" Your number is even but not between 2 and 24 inxclusive \n");
+    }
+    Console.WriteLine(" Would you like to continue \n");
+    string input = Console.ReadLine().ToLower();
+    if (input == "n")
+    {
+        run = false;
+    }
+}
